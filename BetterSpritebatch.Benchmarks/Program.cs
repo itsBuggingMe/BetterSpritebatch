027e3051f0ef13f86110383c662d0be23126351c
itsBuggingMe <lohj002@gmail.com>
2025-01-31 10:02:49 -0500

    add benchmark project

---
+++ BetterSpritebatch.Benchmarks/Program.cs
@@ -0,0 +1,18 @@
+﻿using BenchmarkDotNet;
+using BenchmarkDotNet.Running;
+
+namespace BetterSpritebatch.Benchmarks;
+
+public class Program
+{
+    private Batcher _batcher;
+    static void Main(string[] args)
+    {
+        BenchmarkRunner.Run<Program>();
+    }
+
+    public void Shuffle()
+    {
+
+    }
+}
